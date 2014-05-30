693f2eb01ffaee835dedcef417825f2ab0aaac70
Anton Jongsma <a.jongsma@bitperfect.nl>
2014-05-30 16:28:31 +0200

    fix

---
+++ src/common/heaterd_client.php
@@ -0,0 +1,44 @@
+<?php
+
+define('HEATERD_HOST', 'localhost');
+define('HEATERD_PORT', '7396');
+
+function heaterd_get_state() {
+
+	$fp = fsockopen(HEATERD_HOST, HEATERD_PORT);
+	
+	if($fp === false) {
+		throw new Exception('Cannot read from tempd ('.HEATERD_HOST.':'.HEATERD_PORT.')');
+	}
+
+	fwrite($fp, "get\n");
+
+	return fread($fp);
+}
+
+function heaterd_on() {
+
+	$fp = fsockopen(HEATERD_HOST, HEATERD_PORT);
+	
+	if($fp === false) {
+		throw new Exception('Cannot connect to Heaterd ('.HEATERD_HOST.':'.HEATERD_PORT.')');
+	}
+
+	fwrite($fp, "on\n");
+
+	return true;
+}
+
+function heaterd_off() {
+
+	$fp = fsockopen(HEATERD_HOST, HEATERD_PORT);
+	
+	if($fp === false) {
+		throw new Exception('Cannot connect to Heaterd ('.HEATERD_HOST.':'.HEATERD_PORT.')');
+	}
+
+	fwrite($fp, "off\n");
+
+	return true;
+
+}
