6b8943275ad8a8671c28f6f946e00c3100a18390
Jille Timmermans <jille@karpenoktem.nl>
2014-05-30 17:22:13 +0200

    Updated the API
    * Changed day and night into active and inactive
    * Don't communicate with the manager but update the files directly

--- web/api.php
+++ web/api.php
@@ -10,57 +10,53 @@
 	}
 	switch($_GET['action']) {
 		case 'getTemperatures':
-			$line = shell_exec('tail -1 /var/log/ekroll/temp.log');
-			if(!preg_match('/^(\d+) (\d+\.\d+) (\d+) ([01])$/', $line, $m)) {
-				fail(500, "Internal server error (error parsing temp.log)");
+			$sock = stream_socket_client('tcp://127.0.0.1:7396');
+			$current = floatval(fgets($sock));
+			fclose($sock);
+			$activeTill = intval(file_get_contents('/var/log/ekroll/activeTill.dat'));
+			$active = intval(file_get_contents('/var/log/ekroll/active.dat'));
+			$inactive = intval(file_get_contents('/var/log/ekroll/inactive.dat'));
+			if($activeTill > time()) {
+				$aspired = $active;
+			} else {
+				$aspired = $inactive;
 			}
-			$day = intval(file_get_contents('/var/log/ekroll/day.dat'));
-			$night = intval(file_get_contents('/var/log/ekroll/night.dat'));
-			echo json_encode(array('success' => true, 'current' => $m[2], 'aspired' => $m[3], 'day' => $day, 'night' => $night));
+			echo json_encode(array('success' => true, 'current' => $current, 'aspired' => $aspired, 'active' => $active, 'inactive' => $inactive));
 			exit;
-		case 'setDayTemperature':
-		case 'setNightTemperature':
-		case 'enableDayTill':
-		case 'enableNight':
-		case 'getDayTill':
-			$sock = stream_socket_client('udp://127.0.0.1:7397', $errno, $errstr);
-			if(!$sock) {
-				fail(500, "Internal server error (ctm: ". $errstr .")");
+		case 'setActiveTemperature':
+		case 'setInactiveTemperature':
+			if(!isset($_GET['temperature']) && !ctype_digit($_GET['temperature'])) {
+				fail(400, "Invalid temperature given");
 			}
-			switch($_GET['action']) {
-				case 'setDayTemperature':
-				case 'setNightTemperature':
-					if(!isset($_GET['temperature']) && !ctype_digit($_GET['temperature'])) {
-						fail(400, "Invalid temperature given");
-					}
-					if($_GET['temperature'] < 1 || $_GET['temperature'] > 40) {
-						fail(400, "Param temperature out of range");
-					}
-					fwrite($sock, "ekroll:". $_GET['action'] .":". $_GET['temperature'] .".\n");
-					break;
-				case 'enableDayTill':
-					if(!isset($_GET['timestamp']) && !ctype_digit($_GET['timestamp'])) {
-						fail(400, "Invalid timestamp given");
-					}
-					if($_GET['timestamp'] < time() || $_GET['timestamp'] > time() + 86400) {
-						fail(400, "Param timestamp out of range");
-					}
-					fwrite($sock, "ekroll:enableDayTill:". $_GET['timestamp'] .".\n");
-					break;
-				case 'enableNight':
-					fwrite($sock, "ekroll:enableNight.\n");
-					break;
-				case 'getDayTill':
-					fwrite($sock, "ekroll:getDayTill.\n");
-					$ret = fread($sock, 512);
-					echo json_encode(array('success' => true, 'till' => json_decode($ret)));
-					exit;
+			if($_GET['temperature'] < 1 || $_GET['temperature'] > 40) {
+				fail(400, "Param temperature out of range");
 			}
-			$ret = fread($sock, 512);
-			echo json_encode(array('success' => ($ret == 'OK')));
+			if($_GET['action'] == 'setActiveTemperature') {
+				file_put_contents('/var/log/ekroll/active.dat', $_GET['temperature']);
+			} else {
+				file_put_contents('/var/log/ekroll/inactive.dat', $_GET['temperature']);
+			}
+			echo json_encode(array('success' => true));
+			exit;
+		case 'enableInactive':
+			$_GET['timestamp'] = time();
+			// FALLTHROUGH
+		case 'enableActiveTill':
+			if(!isset($_GET['timestamp']) && !ctype_digit($_GET['timestamp'])) {
+				fail(400, "Invalid timestamp given");
+			}
+			if($_GET['timestamp'] < time() || $_GET['timestamp'] > time() + 86400) {
+				fail(400, "Param timestamp out of range");
+			}
+			file_put_contents('/var/log/ekroll/activeTill.dat', $_GET['timestamp']);
+			echo json_encode(array('success' => true));
+			break;
+		case 'getActiveTill':
+			$ret = intval(file_get_contents('/var/log/ekroll/activeTill.dat'));
+			echo json_encode(array('success' => true, 'till' => json_decode($ret)));
 			exit;
 		default:
-			fail(400, "Invalid action given (getTemperatures, setDayTemperature, setNightTemperature, enableDayTill, enableNight)");
+			fail(400, "Invalid action given (getTemperatures, setActiveTemperature, setInactiveTemperature, enableActiveTill, enableInactive)");
 	}
 
 	function fail($code, $str) {
